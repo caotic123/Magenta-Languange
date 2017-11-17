321d588961d64d85d9ac5040664c525ee2ecf5e4
caotic123 <itotimcampos@windowslive.com>
2017-11-17 03:56:17 -0200

    ok...

--- module.cpp
+++ module.cpp
@@ -341,8 +341,7 @@ void magneta_module::create_if_condition(std::string& cod__, type_ type, std::st
     
     std::pair<int, int> x__n;
     std::get<1>(x__n) = (*__l) + 1;
-	__stack->push_back(x__n);
-
+    __stack->push_back(x__n);
     std::string n = cond__if(x_, __cond[0], __cond[1], type__, operator_, q, __e);
     create_continue_label(x_, q, __l, __stack);
     cond__if(x_, re_load(x_, q, __cond[0], type__), re_load(x_, q, __cond[1], type__), type__, operator_, q, __e); //second condition to eventual loop
